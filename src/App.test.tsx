2d26d1c43898fc5b3bcca3404af6e2f901d5c26d
DimitrisZar <dimitriszarahanis@gmail.com>
2023-01-13 22:37:45 +0200

    init commit

---
+++ src/App.test.tsx
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import App from "./App";
+
+describe("App", () => {
+  test("should render", () => {
+    const { container } = render(<App />);
+    expect(container).toBeTruthy();
+  });
+  test("renders react logo", () => {
+    const { getByAltText } = render(<App />);
+    expect(getByAltText("React logo")).toBeTruthy();
+  });
+});
